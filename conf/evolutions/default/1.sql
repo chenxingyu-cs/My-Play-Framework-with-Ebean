22b924bca7f5a75683a90898e008a0a66e18b07b
Xingyu Chen <chenxingyu.cs@gmail.com>
2016-04-19 17:06:23 -0700

    add two models

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,16 @@
 
 # --- !Ups
 
+create table publication (
+  id                            bigint auto_increment not null,
+  title                         varchar(255),
+  pages                         varchar(255),
+  year                          integer,
+  date                          varchar(255),
+  url                           varchar(255),
+  constraint pk_publication primary key (id)
+);
+
 create table task (
   id                            bigint auto_increment not null,
   name                          varchar(255),
@@ -11,8 +21,21 @@ create table task (
   constraint pk_task primary key (id)
 );
 
+create table user (
+  id                            bigint auto_increment not null,
+  email                         varchar(255),
+  password                      varchar(255),
+  first_name                    varchar(255),
+  last_name                     varchar(255),
+  constraint pk_user primary key (id)
+);
+
 
 # --- !Downs
 
+drop table if exists publication;
+
 drop table if exists task;
+
+drop table if exists user;
 
